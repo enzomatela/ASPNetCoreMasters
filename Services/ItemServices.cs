a665bed768bd47c3d05840927d0a68c3712613df
enzom <EnzoM@magenic.com>
2021-09-16 10:07:12 -0500

    Homework#2

---
+++ Services/ItemServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Services
+{
+    public class ItemServices
+    {
+        public IEnumerable<string> GetAll(int userId)
+        {
+            return Enumerable.Empty<string>();
+        }
+
+
+
+    }
+}
