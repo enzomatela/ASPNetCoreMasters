fa9a0ad1b13ecad3d22e4a52d530495dddcb3eae
enzom <EnzoM@magenic.com>
2021-11-01 22:42:59 -0500

    Custom Validation

---
+++ Masters.Api/Helpers/StringLengthValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Masters.Api.Helpers
+{
+    public class StringLengthValidation  :ValidationAttribute
+    {
+        private int _minLength;
+
+        public StringLengthValidation(int minLength)
+        {
+            _minLength = minLength;
+        }
+
+        public override bool IsValid(object value)
+        {
+            return value.ToString().Length >= _minLength;
+        }
+    }
+}
